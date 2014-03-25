efa399b31bc1c47c5e389d7d4d17d7a94d232ddf
Romain Parage <rparage@talentsoft.com>
2014-03-25 16:41:24 +0100

    Add models

---
+++ CodeAnalysis/Models/CodeMetricsLineModel.cs
@@ -0,0 +1,16 @@
+﻿namespace CodeAnalysis.Models
+{
+    public class CodeMetricsLineModel
+    {
+        public string Scope { get; set; }
+        public string Project { get; set; }
+        public string Namespace { get; set; }
+        public string Type { get; set; }
+        public string Member { get; set; }
+        public double? MaintainabilityIndex { get; set; }
+        public double? CyclomaticComplexity { get; set; }
+        public double? DepthOfInheritance { get; set; }
+        public double? ClassCoupling { get; set; }
+        public double? LinesOfCode { get; set; }
+    }
+}
---
+++ CodeAnalysis/Models/CodeMetricsLineView.cs
@@ -0,0 +1,11 @@
+﻿namespace CodeAnalysis.Models
+{
+    public class CodeMetricsLineView : CodeMetricsLineModel
+    {
+        public double? MaintainabilityIndexDifference { get; set; }
+        public double? CyclomaticComplexityDifference { get; set; }
+        public double? DepthOfInheritanceDifference { get; set; }
+        public double? ClassCouplingDifference { get; set; }
+        public double? LinesOfCodeDifference { get; set; }
+    }
+}
