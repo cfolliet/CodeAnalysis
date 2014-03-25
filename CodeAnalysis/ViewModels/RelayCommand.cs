f51832f2d13655e4700da27b03f169374348c630
Romain Parage <rparage@talentsoft.com>
2014-03-25 19:33:46 +0100

    Implements the browse of metrics files

---
+++ CodeAnalysis/ViewModels/RelayCommand.cs
@@ -0,0 +1,38 @@
+﻿namespace CodeAnalysis.ViewModels
+{
+    using System;
+    using System.Windows.Input;
+
+    /// <summary>
+    /// The class which implements commands
+    /// Example :
+    /// public RelayCommand command = new RelayCommand(param => Action());
+    /// </summary>
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object> _execute;
+        private readonly Predicate<object> _canExecute;
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+    }
+}
--- CodeAnalysis/ViewModels/ViewModel.cs
+++ CodeAnalysis/ViewModels/ViewModel.cs
@@ -1,18 +1,75 @@
 ﻿namespace CodeAnalysis.ViewModels
 {
-    internal class ViewModel : BaseViewModel
+    using Microsoft.Win32;
+
+    public class ViewModel : BaseViewModel
     {
+        #region Constructors
+
         public ViewModel()
         {
-            Text = "lola";
+            BrowseTrunkMetricsFileCommand = new RelayCommand(param => BrowseMetricsFile(true));
+            BrowseBrancheMetricsFileCommand = new RelayCommand(param => BrowseMetricsFile(false));
+            ProceedCommand = new RelayCommand(param => Proceed());
         }
 
-        private string _text;
+        #endregion
 
-        public string Text
+        #region Commands
+
+        public RelayCommand BrowseTrunkMetricsFileCommand { get; set; }
+        public RelayCommand BrowseBrancheMetricsFileCommand { get; set; }
+        public RelayCommand ProceedCommand { get; set; }
+
+        #endregion
+
+        #region Properties
+
+        private string trunkMetricsFilePath;
+
+        public string TrunkMetricsFilePath
         {
-            get { return _text; }
-            set { _text = value; OnPropertyChanged("Text"); }
+            get { return trunkMetricsFilePath; }
+            set { trunkMetricsFilePath = value; OnPropertyChanged("TrunkMetricsFilePath"); }
         }
+
+        private string brancheMetricsFilePath;
+
+        public string BrancheMetricsFilePath
+        {
+            get { return brancheMetricsFilePath; }
+            set { brancheMetricsFilePath = value; OnPropertyChanged("BrancheMetricsFilePath"); }
+        }
+
+        #endregion
+
+        #region Methods
+
+        private void BrowseMetricsFile(bool isTrunk)
+        {
+            var dialog = new OpenFileDialog();
+            bool? result = dialog.ShowDialog();
+
+            if (result == true)
+            {
+                if (isTrunk)
+                {
+                    TrunkMetricsFilePath = dialog.FileName;
+                }
+                else
+                {
+                    BrancheMetricsFilePath = dialog.FileName;
+                }
+            }
+        }
+
+        private void Proceed()
+        {
+            if (!string.IsNullOrWhiteSpace(TrunkMetricsFilePath) && !string.IsNullOrWhiteSpace(BrancheMetricsFilePath))
+            {
+            }
+        }
+
+        #endregion
     }
 }
