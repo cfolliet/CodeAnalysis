f74016342943d3f7762c7734781bc9a7149f0be5
Romain Parage <rparage@talentsoft.com>
2014-03-26 11:20:37 +0100

    Change the background color depending of the difference value

---
+++ CodeAnalysis/Views/BrushConverter.cs
@@ -0,0 +1,36 @@
+﻿namespace CodeAnalysis.Views
+{
+    using System;
+    using System.Windows;
+    using System.Windows.Data;
+    using System.Windows.Media;
+
+    public class BrushConverter : FrameworkElement, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value != null)
+            {
+                int i;
+                Int32.TryParse(value.ToString(), out i);
+
+                if (i < 0)
+                {
+                    return new SolidColorBrush(Colors.Red);
+                }
+
+                if (i > 0)
+                {
+                    return new SolidColorBrush(Colors.Green);
+                }
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
